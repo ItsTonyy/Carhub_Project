8f547e0439dc32449c9d216ca49493185300eed4
Tony <112224948+ItsTonyy@users.noreply.github.com>
2023-12-30 23:58:29 -0300

    starting the car catalogue

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -32,7 +32,7 @@ const Hero = () => {
           <Image src='/hero.png' alt="hero" fill className="object-contain" />
         </div>
 
-        <div className="hero__image-overlay"></div>
+        <div className="hero__image-overlay"/>
       </div>
     </div>
   )
