e18fbf18b5012b964307ffb099454801ebf39a19
Tony <112224948+ItsTonyy@users.noreply.github.com>
2024-01-03 16:46:58 -0300

    Car Catalogue finished

--- components/CarCatalogue/SearchManufacturer.tsx
+++ components/CarCatalogue/SearchManufacturer.tsx
@@ -25,7 +25,7 @@ const SearchManufacturer = ({
   return (
     <div className='search-manufacturer'>
       <Combobox value={manufacturer} onChange={setManufacturer}>
-        <div className='relative w-full ml-16'>
+        <div className='relative w-full'>
           <Combobox.Button className='absolute top-[14px]'>
             <Image
               src='/car-logo.svg'
@@ -39,52 +39,64 @@ const SearchManufacturer = ({
           <Combobox.Input
             className='search-manufacturer__input'
             placeholder='Volkswagen'
-            displayValue={(manufacturer: string) => manufacturer}
+            displayValue={(item: string) => item}
             onChange={(e) => setQuery(e.target.value)}
           />
-        </div>
 
-        <Transition
-          as={Fragment}
-          leave='transition ease-in duration-100'
-          leaveFrom='opacity-100'
-          leaveTo='opacity-0'
-          afterLeave={() => setQuery('')}
-        >
-          <Combobox.Options>
-            {filteredManufacturers.map((item) => (
-              <Combobox.Option
-                key={item}
-                value={item}
-                className={({ active }) => `
-                  relative search-manufacturer__option ${
-                    active
-                      ? 'bg-primary-blue text-white rounded-sm'
-                      : 'text-gray-900'
-                  }`}
-              >
-                {({ selected, active }) => (
-                  <>
-                    <span
-                      className={`block truncate ${
-                        selected ? 'font-medium' : 'font-normal'
-                      }`}
-                    >
-                      {item}
-                    </span>
-                    {selected ? (
-                      <span
-                        className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                          active ? 'text-white' : 'text-teal-600'
-                        }`}
-                      ></span>
-                    ) : null}
-                  </>
-                )}
-              </Combobox.Option>
-            ))}
-          </Combobox.Options>
-        </Transition>
+          <Transition
+            as={Fragment}
+            leave='transition ease-in duration-100'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+            afterLeave={() => setQuery('')}
+          >
+            <Combobox.Options className='search-manufacturer__options' static>
+              {filteredManufacturers.length === 0 && query !== '' ? (
+                <Combobox.Option
+                  value={query}
+                  className='search-manufacturer__option'
+                >
+                  Create "{query}"
+                </Combobox.Option>
+              ) : (
+                filteredManufacturers.map((item) => (
+                  <Combobox.Option
+                    key={item}
+                    className={({ active }) =>
+                      `relative search-manufacturer__option ${
+                        active ? 'bg-primary-blue text-white' : 'text-gray-900'
+                      }`
+                    }
+                    value={item}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected ? 'font-medium' : 'font-normal'
+                          }`}
+                        >
+                          {item}
+                        </span>
+
+                        {/* Show an active blue background color if the option is selected */}
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active
+                                ? 'text-white'
+                                : 'text-pribg-primary-purple'
+                            }`}
+                          ></span>
+                        ) : null}
+                      </>
+                    )}
+                  </Combobox.Option>
+                ))
+              )}
+            </Combobox.Options>
+          </Transition>
+        </div>
       </Combobox>
     </div>
   )
