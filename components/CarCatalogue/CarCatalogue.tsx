e904d77c3fd46965bec8a6e1254b81a56420e824
Tony <112224948+ItsTonyy@users.noreply.github.com>
2024-01-12 23:30:08 -0300

    half way through search, stuck in the manufacturer bug

--- components/CarCatalogue/CarCatalogue.tsx
+++ components/CarCatalogue/CarCatalogue.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import { CustomFilter, SearchBar, CarCard } from '..'
 import { fetchCars } from '../../utils'
+import { HomeProps } from '../../types'
 
-const CarCatalogue = async () => {
-  const allCars = await fetchCars()
+const CarCatalogue = async ({ searchParams }: HomeProps) => {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || '',
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || '',
+    limit: searchParams.limit || 10,
+    model: searchParams.model || '',
+  })
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars
 
--- utils/index.ts
+++ utils/index.ts
@@ -1,14 +1,19 @@
-import { CarProps } from "../types";
+import { CarProps, FilterProps } from '../types'
 
-export async function fetchCars() {
-  const headers =  {
-		'X-RapidAPI-Key': '6f2d3457f1msh675b6d44f4c2f42p12daefjsn328e0d079d4f',
-		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-	}
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel } = filters
 
-  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=a4', {
-    headers: headers
-  })
+  const headers = {
+    'X-RapidAPI-Key': '6f2d3457f1msh675b6d44f4c2f42p12daefjsn328e0d079d4f',
+    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
+  }
+
+  const response = await fetch(
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+    {
+      headers: headers,
+    }
+  )
 
   const result = await response.json()
 
@@ -16,24 +21,24 @@ export async function fetchCars() {
 }
 
 export const calculateCarRent = (city_mpg: number, year: number) => {
-  const basePricePerDay = 50; // Base rental price per day in dollars
-  const mileageFactor = 0.1; // Additional rate per mile driven
-  const ageFactor = 0.05; // Additional rate per year of vehicle age
+  const basePricePerDay = 50 // Base rental price per day in dollars
+  const mileageFactor = 0.1 // Additional rate per mile driven
+  const ageFactor = 0.05 // Additional rate per year of vehicle age
 
   // Calculate additional rate based on mileage and age
-  const mileageRate = city_mpg * mileageFactor;
-  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+  const mileageRate = city_mpg * mileageFactor
+  const ageRate = (new Date().getFullYear() - year) * ageFactor
 
   // Calculate total rental rate per day
-  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate
 
-  return rentalRatePerDay.toFixed(0);
-};
+  return rentalRatePerDay.toFixed(0)
+}
 
 export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   const url = new URL('https://cdn.imagin.studio/getimage')
 
-  const {make, year, model} = car
+  const { make, year, model } = car
 
   url.searchParams.append('customer', 'hrjavascript-mastery')
   url.searchParams.append('make', make)
