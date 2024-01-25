4dbcc5d333bc076782fa4cc213673e84b08cbdb6
Tony <112224948+ItsTonyy@users.noreply.github.com>
2024-01-25 19:02:09 -0300

    Feat: ShowMore finished and working

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -3,11 +3,17 @@
 import { useRouter } from "next/navigation"
 import { showMoreProps } from "../types"
 import { CustomButton } from "."
+import { updateSearchParams } from "../utils"
 
 const ShowMore = ({pageNumber, isNext}: showMoreProps) => {
   const router = useRouter()
 
-  const handleNavigation = () => {}
+  const handleNavigation = () => {
+    const newLimit = (pageNumber + 1) * 10
+    const newPathName = updateSearchParams('limit', `${newLimit}`)
+
+    router.push(newPathName, {scroll: false})
+  }
 
   return (
     <div className="w-full flex-center gap-5 mt-10">
@@ -15,7 +21,7 @@ const ShowMore = ({pageNumber, isNext}: showMoreProps) => {
         <CustomButton 
           title="Show More"
           btnType="button"
-          containerStyles="bg-primary-blue rounded-full text-white"
+          containerStyles="bg-primary-blue rounded-full text-white hover:bg-blue-700 hover:font-semibold focus:bg-blue-800 font-medium "
           handleClick={handleNavigation}
         />
       )}
